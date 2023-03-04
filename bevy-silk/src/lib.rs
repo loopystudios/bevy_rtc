5ed105540a2194dfa545bf93b73f80a5fc072fa3
Spencer C. Imbleau <spencer@imbleau.com>
2023-03-04 11:32:18 -0500

    compiles

--- bevy-silk/src/lib.rs
+++ bevy-silk/src/lib.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
-
 pub struct SilkClientPlugin;
 
-impl Plugin for SilkClientPlugin {}
+impl Plugin for SilkClientPlugin {
+    fn build(&self, app: &mut App) {
+        todo!()
+    }
+}
