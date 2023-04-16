10b799f886cdec85ecd64aa472713a5b07551b48
seabassjh <sebjfk@gmail.com>
2023-04-15 21:43:53 -0400

    remove old packets

--- silk-common/src/demo_packets.rs
+++ silk-common/src/demo_packets.rs
@@ -2,24 +2,6 @@ use crate::{packet::SilkPacket, router::Message};
 use bevy_matchbox::matchbox_socket::Packet;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
-pub enum PaintingDemoPayload {
-    Chat { from: String, message: String },
-    DrawPoint { x1: f32, y1: f32, x2: f32, y2: f32 },
-}
-
-impl From<PaintingDemoPayload> for Packet {
-    fn from(value: PaintingDemoPayload) -> Self {
-        bincode::serialize(&value).unwrap().into_boxed_slice()
-    }
-}
-
-impl From<Packet> for PaintingDemoPayload {
-    fn from(value: Packet) -> Self {
-        bincode::deserialize(&value).unwrap()
-    }
-}
-
 #[derive(Deserialize, Serialize, Default, Clone, Debug)]
 pub struct DrawPoint {
     pub x1: f32,
