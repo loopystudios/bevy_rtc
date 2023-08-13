2137dc5cfc281023085a155cfdb7f1b5d22739ef
Spencer C. Imbleau <spencer@imbleau.com>
2023-08-12 23:28:15 -0400

    using json instead of bincode

--- silk-net/src/lib.rs
+++ silk-net/src/lib.rs
@@ -1,4 +1,5 @@
 use bevy_matchbox::matchbox_socket::Packet;
+use log::{error, warn};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::fmt::Debug;
 
@@ -17,10 +18,25 @@ pub trait Payload:
     fn reflect_name() -> &'static str;
 
     fn from_packet(packet: &Packet) -> Option<Self> {
-        bincode::deserialize::<SilkPacket<Self>>(packet)
-            .ok()
-            .filter(|silk_packet| silk_packet.msg_id == Self::id())
-            .map(|silk_packet| silk_packet.data)
+        let result: Result<SilkPacket<Self>, serde_json::Error> =
+            serde_json::from_slice(packet);
+        match result {
+            Ok(packet) => {
+                if packet.msg_id == Self::id() {
+                    Some(packet.data)
+                } else {
+                    None
+                }
+            }
+            Err(e) => {
+                error!("failed to deserialize packet! err: {e:?}, bytes: {packet:?}");
+                None
+            }
+        }
+        //bincode::deserialize::<SilkPacket<Self>>(packet)
+        //    .ok()
+        //    .filter(|silk_packet| silk_packet.msg_id == Self::id())
+        //    .map(|silk_packet| silk_packet.data)
     }
 
     fn to_packet(&self) -> Packet {
@@ -28,6 +44,15 @@ pub trait Payload:
             msg_id: Self::id(),
             data: self.clone(),
         };
-        bincode::serialize(&silk_packet).unwrap().into_boxed_slice()
+
+        let input_string = serde_json::to_string(&silk_packet).unwrap();
+
+        // Convert the string to bytes
+        let bytes = input_string.as_bytes();
+
+        // Allocate a Box to hold the bytes
+        let boxed_bytes: Box<[u8]> = bytes.into();
+        //bincode::serialize(&silk_packet).unwrap().into_boxed_slice()
+        boxed_bytes
     }
 }
